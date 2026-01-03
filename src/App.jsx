1cd995b335f266ced64bf4578ebdb3c80355cadb
kapilraj10 <kapilrajkc10@gmail.com>
2026-01-03 21:44:25 +0545

    app.jsx and package.josn update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import Login from './pages/login/Login'
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Toaster } from 'react-hot-toast'
+import Login from "./pages/login/Login"
 
 const App = () => {
-  return <Login />
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Toast Container */}
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            fontSize: '14px',
+          },
+        }}
+      />
+    </>
+  )
 }
 
 export default App
